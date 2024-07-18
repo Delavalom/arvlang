074032790f3813b79c35cf3772aa8763cc714f25
Delavalom <hi@delavalom.com>
2024-07-18 10:53:45 -0400

    chore: Add syntax package with keywords and operators

---
+++ lang/syntax/syntax.go
@@ -0,0 +1,23 @@
+package syntax
+
+var Keywords = []string{
+	"true",
+	"false",
+
+	"if",
+	"else",
+	"for",
+	"break",
+	"continue",
+
+	"return",
+
+	"fn",
+
+	"module",
+}
+
+var Operators = []string{
+	"&&",
+	"||",
+}
