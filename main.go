990f689589af627b8b86cf120bfdf3e8ee621d8c
Delavalom <hi@delavalom.com>
2024-08-14 10:41:34 -0400

    repl tested with monkey pogramming language implementation

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/user"
+
+	"github.com/delavalom/arvlang/lang/newlexer"
 )
 
 func main() {
-	os.Exit(0)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Arv programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	newlexer.Start(os.Stdin, os.Stdout)
 }
