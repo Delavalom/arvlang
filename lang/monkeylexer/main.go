1af7dea5df1e5db2a2e61ebfef4afbdc2d8dcaed
Delavalom <hi@delavalom.com>
2024-09-03 01:02:47 -0400

    extend lexer

--- lang/monkeylexer/main.go
+++ lang/monkeylexer/main.go
@@ -1,9 +1,11 @@
-package monkeylexer
+package main
 
 import (
 	"fmt"
 	"os"
 	"os/user"
+
+	"github.com/delavalom/arvlang/lang/monkeylexer/repl"
 )
 
 func main() {
@@ -13,5 +15,5 @@ func main() {
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
-	Start(os.Stdin, os.Stdout)
+	repl.Start(os.Stdin, os.Stdout)
 }
